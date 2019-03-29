1b76426e8ca2a77689ab7f009327fe95048524f2
mikhail <vms2108@mail.ru>
2019-03-29 10:43:00 +0300

    addition table

---
+++ src/router.js
@@ -0,0 +1,15 @@
+// router.js
+import Vue from 'vue'
+import Router from 'vue-router'
+
+Vue.use(Router)
+
+export default new Router({
+    routes: [
+      {
+        path: '/',
+        name: 'home',
+        component: home
+      }
+    ]
+})
